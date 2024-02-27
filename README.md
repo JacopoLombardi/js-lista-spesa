75ebb2a6b29126fe5250d85067f3a329f9eb6da0
JacopoLombardi <lombajacopo@gmail.com>
2024-02-27 14:18:00 +0100

    [add] adjust & added Readme file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# js-lista-spesa
+lista della Spesa
+===
+1. Creo un Array con all'interno vai elementi.
+2. Creo e assegno su Js una variabile collegata sull'HTML.
+3. Imposto un contatore per il ciclo While.
+4. Creo un ciclo While con un contatore, il quale al suo incremento il ciclo stamperà sull'HTML un nuovo elemento 'li' con all'interno l'Array con indice il valore del contatore corrente.
--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
+
+// Array con la lista della spesa
+const listaSpesa = [
+   'elemento_1', 
+   'elemento_2', 
+   'elemento_3', 
+   'elemento_4', 
+   'elemento_5', 
+   'elemento_6'
+];
+
 // assegno su Js una variabile dell'HTML
 const output = document.getElementById('output');
 
-
-// Array con la lista della spesa
-const listaSpesa = ['elemento1', 'elemento2', 'elemento3', 'elemento4', 'elemento5', 'elemento6'];
-
-
+// contatore del ciclo While
 counter = 0;
 
 
 // ciclo While che cicle finchè non raggiunge l'ultimo elemento
 while(counter < listaSpesa.length){
-
-   output.innerHTML += listaSpesa[counter];
+   output.innerHTML += `<li class="fs-4">${listaSpesa[counter]}</li>`;  // stampo sull'HTML uno ad uno gli elementi dell'Array 
    counter++;
-   
 }
